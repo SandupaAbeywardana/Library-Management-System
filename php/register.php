b1fdad277f748d00072506339995f87d7b833f4f
HunTrix007 <sachinthakaa86@gmail.com>
2022-07-23 17:06:35 +0530

    php to check if already acc exists in register.php

--- php/register.php
+++ php/register.php
@@ -6,15 +6,34 @@
     $Email = $_POST['email'];
     $Password = $_POST['pwd'];
 
-    $SQL = "INSERT INTO user_accounts (Email, FirstName, LastName, user_Password) VALUES ('$Email', '$FirstName', '$LastName', '$Password')";
+    $exist = mysqli_query($conn,"SELECT * FROM user_accounts where Email='$email'");
+    $row  = mysqli_fetch_array($exist);
+    if(is_array($row)){
 
-    if(mysqli_query($conn,$SQL)){
-		echo "<script type='text/javascript'>alert('Successfully registered')</script>";
-        echo('<script>window.location.replace("../HTML/login.html");</script>');
+        $SQL = "INSERT INTO user_accounts (Email, FirstName, LastName, user_Password) VALUES ('$Email', '$FirstName', '$LastName', '$Password')";
+
+        if(mysqli_query($conn,$SQL)){
+            echo ("<script type='text/javascript'>alert('Successfully registered')</script>");
+            echo('<script>window.location.replace("../HTML/login.html");</script>');
+        }
+        else{
+            echo "<script>alert ('Registration unsuccessful.<br>')</script>";
+        }
+
     }
-	else{
-		echo "<script>alert ('Registration unsuccessful.<br>')</script>";
-	}
+    else{
+        echo(
+            "<script>
+                if(confirm('Email already exist.  Login?') == true) {
+                    window.location.replace('../HTML/login.html');
+                }
+                else{
+                    window.location.replace('../HTML/register.html');
+                }
+            </script>"
+        );
+        
+    }
 	
 	mysqli_close($conn);
 
